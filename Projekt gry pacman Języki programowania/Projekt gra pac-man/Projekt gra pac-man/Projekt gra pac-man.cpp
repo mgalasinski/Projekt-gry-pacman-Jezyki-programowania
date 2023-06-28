c3202597909e9638f0b2485c072889c834df72bf
mgalasinski <137840901+mgalasinski@users.noreply.github.com>
2023-06-28 05:40:45 +0200

    Add files via upload

---
+++ Projekt gry pacman Języki programowania/Projekt gra pac-man/Projekt gra pac-man/Projekt gra pac-man.cpp
@@ -0,0 +1,12 @@
+﻿// Projekt gra pac-man.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "Projekt gra pac-man.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
